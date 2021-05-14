d21eefdce7cbf828620ed52050603eae6f4df531
Ioshiken <68305610+Ioshiken@users.noreply.github.com>
2021-05-13 21:29:14 -0300

    comite total JS
    Atualizacaod e JS com HTML atualizado

---
+++ HTML_EXERC/jsproject/Exerc2/script.js
@@ -0,0 +1,11 @@
+let vetor =[1,3,5,7,9,11,13];
+
+let numero=vetor[1] +vetor[3];
+
+console.log(numero>9);
+console.log("-------");
+console.log((vetor[6] - numero) == 5 || true);
+console.log("-------");
+console.log(true == "true");
+console.log("--------");
+console.log(true ==="true");
---
+++ HTML_EXERC/jsproject/exerc1/script.js
@@ -0,0 +1,12 @@
+let valor1=10;
+let valor2=20;
+let valor3="3";
+function somarValores(primeiro, segundo){
+console.log(primeiro +segundo)
+}
+
+//O que ocorrera nos seguintes casos
+somarValores(9,valor1);
+somarValores(valor1,valor2);
+somarValores(valor2, valor3);
+somarValores("100", valor3);
