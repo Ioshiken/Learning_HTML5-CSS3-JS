0106c789256f09145f6bb45076936a8345240eee
Ioshiken <68305610+Ioshiken@users.noreply.github.com>
2021-05-17 18:27:35 -0300

    commite All HTML

---
+++ HTML_EXERC/gustavologica/exerc2/script.js
@@ -0,0 +1,4 @@
+var n1= Number.parseFloat(window.prompt('Digite um numero')) // o comando prompt recebe string
+var n2= Number.parseFloat(window.prompt('Digite outro numero'))
+var s=n1+n2
+window.alert(`A soma entre ${n1} e ${n2} e igual a ${s}`) // ou pode conventer com comando String (s) declaracao na versao mais nova do JS
---
+++ HTML_EXERC/gustavologica/exerc3/script.js
@@ -0,0 +1,2 @@
+var nome = window.prompt('Qual e seu nome?')
+document.white(`Seu nome tem ${nome.length} letras`)
