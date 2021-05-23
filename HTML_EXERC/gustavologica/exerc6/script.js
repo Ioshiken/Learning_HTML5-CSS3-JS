0722feeb1ada8b3ffdbeb8b8389d255ebb55c8ee
Ioshiken <68305610+Ioshiken@users.noreply.github.com>
2021-05-22 23:56:52 -0300

    commit

--- HTML_EXERC/gustavologica/exerc6/script.js
+++ HTML_EXERC/gustavologica/exerc6/script.js
@@ -1,4 +1,13 @@
+var a = window.document.getElementById('area');
 function clicar(){
-    var a = window.document.getElementById('area');
+   
     a.innerText = 'Clicou!'
+    a.style.background ='red'
+}
+function entrar(){
+    a.innerText ='Entrou!'
+}
+function sair(){
+    a.innerText ='Saiu!'
+    a.style.background ='green'
 }
