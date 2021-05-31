40f34ad19a9689a8de2c09f2133cec3903ee3e25
Ioshiken <68305610+Ioshiken@users.noreply.github.com>
2021-05-30 23:43:19 -0300

    comite 30 05

---
+++ HTML_EXERC/jsproject/exerc3/script.js
@@ -0,0 +1,10 @@
+//window.console.log("Eu sou o console")
+//window.console.log(
+//    window.document.getElementById("titulo").innerHTML
+  //  )
+  //window.console.log(
+  //window.screen.width
+ // )
+ window.console.log(
+    window
+    )
